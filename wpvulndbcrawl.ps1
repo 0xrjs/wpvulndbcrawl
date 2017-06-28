6cd395803a9629ee0dc2ecb0dfcdc410c54c368b
0xrjs <0xrjs@users.noreply.github.com>
2017-06-28 09:51:35 -0500

    Merged code from to build search index
    https://github.com/RubberDucky1106/Scripts/blob/master/buildWPindex.ps1

--- wpvulndbcrawl.ps1
+++ wpvulndbcrawl.ps1
@@ -39,3 +39,49 @@ For ($vulnIndex=0; $vulnIndex -le $vulnCount; $vulnIndex++)
 
 # display the final parsed data
 $vulns | Select-Object -Property * -ExcludeProperty references
+
+# build serach index to aid users in their queries
+Function Build_Index
+{
+	param
+	(
+		[Parameter(Mandatory=$True,Position=1)]
+		[ValidateSet("plugins","themes")]
+		[string]$Index
+	)
+	
+	#instatiate arraylist to contain all plugins/themes in loop
+	[System.Collections.ArrayList]$extractionsArray = @()
+	#count to iterate through pages via URI
+	$count = 1
+	#flag for loop exit
+	$nullPage = $False
+	Do
+	{
+		#retrieve html
+		$html = Invoke-WebRequest -Uri https://wpvulndb.com/$($Index)?page=$($count)
+		
+		#extract from the links all plugins or themes
+		$extractions = ($html.links.href | Select-String -Pattern "/$($Index)/") | % {$_.tostring().split('/')[2]}
+		
+		#if there are no plugins or themes (last page), set exit flag true to exit loop
+		If ($extractions -eq $Null)
+		{
+			$nullPage = $True
+		}
+		#add the extracted plugins/themes to the arraylist. Out-null because adding items to array has terminal output. increment page count by one.
+		Else
+		{
+			Foreach ($link in $extractions)
+			{
+				$extractionsArray.Add($link) | Out-Null
+			}
+			$count++
+		}
+	}
+	Until ($nullpage -eq $True)
+
+	# take arraylist and sort alphabetically removing duplicates. output to text file.
+	$extractionsArray | Sort-Object -Unique | Out-File -Path #***WHERE?***
+
+}
